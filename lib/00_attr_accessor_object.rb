e6eb493e50735726e2ed4c31eba23c22343415bc
Tu Ngo <tungo8810@gmail.com>
2017-04-14 10:43:45 -0700

    Add AttrAccessorObject class

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,17 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    
+    names.each do |name|
+      get = name.to_sym
+      set = "#{name}=".to_sym
+      attr = "@#{name}"
+
+      define_method(get) do
+        instance_variable_get(attr)
+      end
+
+      define_method(set) do |value|
+        instance_variable_set(attr, value)
+      end
+    end
   end
 end
